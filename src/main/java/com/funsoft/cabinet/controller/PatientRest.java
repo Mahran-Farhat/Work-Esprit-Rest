d2e5e59785b89e566375de1a7ec53e6e8dc987ef
Mahran-Farhat <farhatmahran@gmail.com>
2021-11-05 20:20:44 +0100

    display list

--- src/main/java/com/funsoft/cabinet/controller/PatientRest.java
+++ src/main/java/com/funsoft/cabinet/controller/PatientRest.java
@@ -5,9 +5,12 @@ import com.funsoft.cabinet.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 public class PatientRest {
@@ -27,6 +30,24 @@ public class PatientRest {
     @RequestMapping(value = "/clients/save",method = RequestMethod.POST)
     public ModelAndView save(@ModelAttribute("FormClient") Client client){
         agent.saveorupdate(client);
-        return new ModelAndView("redirect:/");
+        return new ModelAndView("redirect:/clients/list");
+    }
+
+    @RequestMapping(value = "/clients/list",method = RequestMethod.GET)
+    public ModelAndView liste_clients(){
+        List<Client> clients = agent.consulte();
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("clients",clients);
+        model.setViewName("mes_clients");
+        return model;
+    }
+
+    @RequestMapping(value = "/clients/delete/{id}",method = RequestMethod.GET)
+    public ModelAndView delete_client(@PathVariable("id") long id){
+
+        agent.delete(id);
+        return new ModelAndView("redirect:/clients/list");
+
     }
 }
